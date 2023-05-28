247cc6766dfb3a28cd177447e26b8df58f35a31e
MrShnaider <alex-fktrc@yandex.ru>
2023-05-29 02:36:20 +0300

    feat: улучшает UX работы с полями и отправкой

--- front/src/components/FieldWithTitle.vue
+++ front/src/components/FieldWithTitle.vue
@@ -1,9 +1,9 @@
 <template>
-	<label>
-		<span class="block text-[#606F78] text-2xl mb-1">{{ title }}</span>
+	<div>
+		<h2 class="block text-[#606F78] text-2xl mb-1">{{ title }}</h2>
 		<hr class="border border-gray-400 mb-3">
 		<slot />
-	</label>
+	</div>
 </template>
 
 <script setup lang="ts">
--- front/src/pages/CreateEntityPage.vue
+++ front/src/pages/CreateEntityPage.vue
@@ -6,21 +6,26 @@
 			</field-with-title>
 			<div>
 				<field-with-title title="Поля">
-					<div v-for="field in fields" class="grid grid-cols-1 items-center" :key="field.id">
-						<div style="grid-column: 1/1; grid-row: 1/1;" class="grid grid-cols-12 gap-3">
-							<text-input v-model:text="field.title" class="col-span-8" />
-							<text-input v-model:text="field.type" class="col-span-4" />
+					<div class="grid gap-1">
+						<div v-for="field in fields" class="grid grid-cols-1 items-center" :key="field.id">
+							<div style="grid-column: 1/1; grid-row: 1/1;" class="grid grid-cols-12 gap-3">
+								<text-input v-model:text="field.title" class="col-span-8" v-focus />
+								<text-input v-model:text="field.type" @keyup.enter="addField" class="col-span-4" />
+							</div>
+							<button
+								@click="removeField(field.id)"
+								style="grid-column: 1/1; grid-row: 1/1;"
+								class="justify-self-end -mr-6"
+							>X</button>
 						</div>
-						<button @click="removeField(field.id)" style="grid-column: 1/1; grid-row: 1/1;"
-							class="justify-self-end -mr-6">X</button>
+						<button class="block min-w-full" @click="addField">
+							<span class="
+								flex justify-center min-w-full py-1 px-2 rounded-lg
+								text-gray-500 border-2 border-gray-300 bg-gray-100
+							">Добавить поле</span>
+						</button>
 					</div>
 				</field-with-title>
-				<button class="block min-w-full mt-3" @click="addField">
-					<span class="
-						flex justify-center min-w-full py-1 px-2 rounded-lg
-						text-gray-500 border-2 border-gray-300 bg-gray-100
-					">Добавить поле</span>
-				</button>
 			</div>
 			<div class="flex justify-end">
 				<primary-button @click="createNewEntity">Сгенерировать</primary-button>
@@ -48,6 +53,9 @@ interface EntityField {
 const fields = ref<EntityField[]>([]);
 let currentFieldId = 1;
 const entityName = ref('');
+const vFocus = {
+	mounted: (el: HTMLInputElement) => el.focus()
+}
 
 const addField = () => {
 	fields.value.push({ id: currentFieldId++, title: '', type: '' });
@@ -58,11 +66,17 @@ const removeField = (id: number) => {
 }
 
 const createNewEntity = async () => {
+	const allFieldsAreFilled = fields.value.reduce((allPrevAreFilled, field) => allPrevAreFilled && field.title !== '' && field.type !== '', true);
+	if (!allFieldsAreFilled || entityName.value === '') {
+		alert('Не все поля заполнены');
+		return;
+	}
 	await axios.post('http://clean-generator/api/entity/create', {
 		root_directory: project.currentProject?.directory,
 		entity_name: entityName.value,
 		fields: fields.value,
 	});
+	alert('Сущность ' + entityName.value + ' сгенерирована');
 	entityName.value = '';
 	fields.value = [];
 }
